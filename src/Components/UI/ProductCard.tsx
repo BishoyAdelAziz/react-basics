699e8f4455ac7c1a504e0f31978143c7512fe62a
Bishoy Adel <bishoy.adel5555@gmail.com>
2025-02-28 17:35:21 +0200

    Enhance ProductCard component with improved layout and dynamic alert message

--- src/Components/UI/ProductCard.tsx
+++ src/Components/UI/ProductCard.tsx
@@ -6,12 +6,17 @@ interface IProps {
   product: IProduct;
 }
 const ProductCart = ({ product }: IProps) => {
-  const { title, description, imageURL, price, category } = product;
+  const { title, description, imageURL, price, category, id } = product;
   return (
-    <div className="border rounded-md p-2 flex flex-col">
-      <ProductImage alt="Car" className="rounded-md " ImageURL={imageURL} />
-      <h3>{title}</h3>
-      <p>{textSlicer(description)}</p>
+    <div
+      key={id}
+      className="border rounded-md p-2 flex flex-col gap-3 justify-between"
+    >
+      <div className="h-min-[50%]">
+        <ProductImage alt="Car" ImageURL={imageURL} />
+      </div>
+      <h3 className="text-2xl">{title}</h3>
+      <p className="text-sm">{textSlicer(description)}</p>
       <div className="flex items-center space-x-2">
         <span className="w-5 h-5 rounded-full bg-indigo-600 cursor-pointer" />
         <span className="w-5 h-5 rounded-full bg-yellow-600 cursor-pointer" />
@@ -26,9 +31,9 @@ const ProductCart = ({ product }: IProps) => {
           className="w-10 h-10 rounded-full"
         />
       </div>
-      <div className="flex items-center justify-between space-x-2 mt-5">
+      <div className="flex items-center justify-between space-x-2">
         <Button
-          onClick={() => alert("Hello")}
+          onClick={() => alert(`you Need to Edit ${title}`)}
           className="bg-indigo-700 hover:bg-indigo-900"
           width="w-full"
         >
